9b7c981b6a17be54bac1b613f67e90c9bfbdcb04
TheFSilver <dasylva.f@gmail.com>
2018-08-17 01:30:28 +0200

    Full mailer commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
   end
 
   def newsletter
+    NewsletterMailer.contact(Mchimp.new.collect_mails).deliver_now
+    PoitiersMailer.pcontact(CsvConvert.new.mailspoitiers).deliver_now
+    NanterreMailer.ncontact(CsvConvert.new.mailsnanterre).deliver_now
+    CultureMailer.cultural(CsvConvert.new.mails_insti_cult).deliver_now
   end
 end
