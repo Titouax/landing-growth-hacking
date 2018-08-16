88fc3fa22fd0a6eba0c61a545ef06b86a76a9eb9
TheFSilver <dasylva.f@gmail.com>
2018-08-16 17:16:01 +0200

    route update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   get 'pages/home'
+  get 'pages/hometwo'
+  get 'pages/hometree'
   get 'pages/newsletter'
 
   root 'pages#home'
-<<<<<<< HEAD
   get 'newsletter', to: 'pages#newsletter'
-=======
-  get 'pages/hometwo'
-  get 'pages/hometree'
   # resources :pages
->>>>>>> master
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
