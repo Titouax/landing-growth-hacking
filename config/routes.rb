61a6959cca57d1f8d26f41627d6217111bec5fbb
Cathleen HENNINOT <cathleenhenninot@MacBook-Pro-de-Cathleen.local>
2018-08-16 15:04:23 +0200

    creation des routes + homepage 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/home'
   root 'pages#home'
+  get 'pages/hometwo'
+  get 'pages/hometree'
+  # resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
