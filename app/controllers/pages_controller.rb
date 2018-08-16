438628ab1d0decf986adc8d6841802fab506b124
TheFSilver <dasylva.f@gmail.com>
2018-08-16 16:41:56 +0200

    controller update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+  end
+
+  def newsletter
     NewsletterMailer.contact(Mchimp.new.collect_mails).deliver_now
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/home'
+  get 'pages/newsletter'
+  get 'pages/newsletter'
+  get 'pages/home'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
