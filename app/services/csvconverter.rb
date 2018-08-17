6f4f03429664556d37e0c20147388d1f64fe9201
Titouan Dessus <33919562+Titouax@users.noreply.github.com>
2018-08-17 03:56:51 +0200

    Update csvconverter.rb

--- app/services/csvconverter.rb
+++ app/services/csvconverter.rb
@@ -48,10 +48,8 @@ class CsvConvert
     return @emailnanterre
   end
 end
-<<<<<<< HEAD
-=======
 
 #CsvConvert.new.mails_insti_cult
 #CsvConvert.new.mailspoitiers
 #puts CsvConvert.new.mailsnanterre
->>>>>>> master
+
